3b7cee6b02f15129a26f4ab6df1120e560f13ebc
maksym <themax087@gmail.com>
2022-04-23 23:13:35 +0300

    copy style in xls

--- excel.py
+++ excel.py
@@ -4,7 +4,7 @@ from openpyxl import load_workbook
 from openpyxl.styles import PatternFill
 from xlrd import open_workbook
 from xlutils.copy import copy
-from xlwt import easyxf
+from xlwt import XFStyle
 
 
 class XlsFile:
@@ -36,13 +36,74 @@ class XlsFile:
         return sheet.cell_value(rowx=row, colx=col)
 
     def copy_with_highlights(self, path, highlights_by_sheet):
-        style = easyxf('pattern: pattern solid, fore_colour red')
         new_wb = copy(self.book)
         for name, highlights in highlights_by_sheet.items():
+            old_sheet = self.sheet_by_name(name)
+            sheet = new_wb.get_sheet(name)
             for row, col in highlights:
-                sheet = new_wb.get_sheet(name)
-                sheet.write(row, col, self.cell_value(name, row, col), style)
+                old_style = self.book.xf_list[old_sheet.cell(row, col).xf_index]
+                sheet.write(
+                    row, col, self.cell_value(name, row, col),
+                    self.copy_style_with_red_back(old_style),
+                )
         new_wb.save(path)
+
+    def copy_style_with_red_back(self, rdxf):
+        wtxf = XFStyle()
+
+        wtxf.pattern.pattern = 1
+        wtxf.pattern.pattern_fore_colour = 10
+        wtxf.pattern.pattern_back_colour = 60
+
+        wtxf.num_format_str = self.book.format_map[rdxf.format_key].format_str
+
+        wtf = wtxf.font
+        rdf = self.book.font_list[rdxf.font_index]
+        wtf.height = rdf.height
+        wtf.italic = rdf.italic
+        wtf.struck_out = rdf.struck_out
+        wtf.outline = rdf.outline
+        wtf.shadow = rdf.outline
+        wtf.colour_index = rdf.colour_index
+        wtf.bold = rdf.bold
+        wtf._weight = rdf.weight
+        wtf.escapement = rdf.escapement
+        wtf.underline = rdf.underline_type
+        wtf.family = rdf.family
+        wtf.charset = rdf.character_set
+        wtf.name = rdf.name
+
+        wtp = wtxf.protection
+        rdp = rdxf.protection
+        wtp.cell_locked = rdp.cell_locked
+        wtp.formula_hidden = rdp.formula_hidden
+
+        wtb = wtxf.borders
+        rdb = rdxf.border
+        wtb.left = rdb.left_line_style
+        wtb.right = rdb.right_line_style
+        wtb.top = rdb.top_line_style
+        wtb.bottom = rdb.bottom_line_style
+        wtb.diag = rdb.diag_line_style
+        wtb.left_colour = rdb.left_colour_index
+        wtb.right_colour = rdb.right_colour_index
+        wtb.top_colour = rdb.top_colour_index
+        wtb.bottom_colour = rdb.bottom_colour_index
+        wtb.diag_colour = rdb.diag_colour_index
+        wtb.need_diag1 = rdb.diag_down
+        wtb.need_diag2 = rdb.diag_up
+
+        wta = wtxf.alignment
+        rda = rdxf.alignment
+        wta.horz = rda.hor_align
+        wta.vert = rda.vert_align
+        wta.dire = rda.text_direction
+        wta.rota = rda.rotation
+        wta.wrap = rda.text_wrapped
+        wta.shri = rda.shrink_to_fit
+        wta.inde = rda.indent_level
+
+        return wtxf
 
 
 class XlsxFile:
